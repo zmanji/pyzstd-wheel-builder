3100f847019a5d707ff765689f2a80d258e12b4a
Zameer Manji <zmanji@gmail.com>
2022-05-13 03:30:33 +0000

    fix typo again

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ def main():
     subprocess.run(["./venv/bin/pip", "install", "-U", "wheel==0.37.1", "pip==22.0.4", "setuptools==62.2.0"])
 
     shutil.rmtree("./src", ignore_errors=True)
-    tf = tarfile.open(name="./0.15.2.gz")
+    tf = tarfile.open(name="./0.15.2.tar.gz")
     tf.extractall("./src")
     tf.close()
 
