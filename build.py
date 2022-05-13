46c689033d23b9a8f3cf840a710ea4c1ffab79c8
Zameer Manji <zmanji@gmail.com>
2022-05-13 03:41:27 +0000

    build wheel

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+import datetime
+import os
 import venv
 import subprocess
 import shutil
 import tarfile
+
+VERSION = "0.15.2"
 
 def main():
     builder = venv.EnvBuilder(
@@ -12,16 +16,21 @@ def main():
             )
 
     builder.create("./venv")
-    subprocess.run(["./venv/bin/pip", "install", "-U", "wheel==0.37.1", "pip==22.0.4", "setuptools==62.2.0"])
+    subprocess.run(["./venv/bin/pip", "install", "-U", "wheel==0.37.1", "pip==22.0.4", "setuptools==62.2.0"], check=True)
 
     shutil.rmtree("./src", ignore_errors=True)
-    tf = tarfile.open(name="./0.15.2.tar.gz")
+    tf = tarfile.open(name=f"./{VERSION}.tar.gz")
     tf.extractall("./src")
     tf.close()
 
+    python = os.path.abspath("./venv/bin/python3")
 
-def run(args):
-    subprocess.run(args, check=True)
+    os.chdir(f"/src/pyzstd-{VERSION}")
+
+
+    d = datetime.now()
+
+    subprocess.run([python, "./setup.py", "bdist_wheel", "-dynamic-link-zstd", "--build-number", d.isoformat(timespec='minutes')], check=True, env={'SOURCE_DATE_EPOCH': "315532800", "CFLAGS": "-g0 -march=x86-64-v3 -O3"})
 
 
 if __name__ == '__main__':
